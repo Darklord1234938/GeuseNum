160eeacadaf4361e04aa29dbe18a003cdb58e025
Darklord1234938 <117258557+Darklord1234938@users.noreply.github.com>
2025-02-22 09:56:20 +0100

    r
    r

---
+++ RandomNum/RandomNum/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace RandomNum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ToLable = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(515, 97);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Geus The Number";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Agency FB", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(14, 153);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(564, 39);
+            this.textBox1.TabIndex = 1;
+            // 
+            // ToLable
+            // 
+            this.ToLable.AutoSize = true;
+            this.ToLable.Font = new System.Drawing.Font("Agency FB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ToLable.Location = new System.Drawing.Point(40, 106);
+            this.ToLable.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ToLable.Name = "ToLable";
+            this.ToLable.Size = new System.Drawing.Size(177, 35);
+            this.ToLable.TabIndex = 2;
+            this.ToLable.Text = "Geus The Number";
+            this.ToLable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ToLable.Click += new System.EventHandler(this.ToLable_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Agency FB", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(240, 198);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 250);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ToLable);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Number Geuser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label ToLable;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RandomNum/RandomNum/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RandomNum
+{
+    public partial class Form1 : Form
+    {
+        int highestNum = 10, rndNums, turns;
+        float points;
+        bool once = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ToLable.Text = "What Is Highest Number You Want?";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "".Trim())
+            {
+                if (once)
+                {
+                    if (int.TryParse(textBox1.Text, out int j))
+                    {
+                        highestNum = j;
+                        once = false;
+                        Random random = new Random();
+                        rndNums = random.Next(0, highestNum + 1);
+                    }
+                }
+                else
+                {
+                    if (int.TryParse(textBox1.Text, out int j))
+                    {
+                        if (j > rndNums)
+                        {
+                            ToLable.Text = $"{j} Is To High";
+                        }
+                        else if (j < rndNums)
+                        {
+                            ToLable.Text = $"{j} Is To Low";
+                        }
+                        else
+                        {
+                            ToLable.Text = "You Won. What Is Highest Number You Want?";
+                            once = true;
+                        }
+                    }
+                }
+                textBox1.Text = "";
+            }
+        }
+
+        private void ToLable_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
